9ad650e507faaae98669e6eed04fd312a5fe96ed
Rafael Lucena <berlimrafa.rf@gmail.com>
2025-06-09 23:35:31 -0300

    Criando script.js

---
+++ meu-site/js/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Mensagem de teste no console
+console.log("JavaScript funcionando!");
+
+// Exemplo de interação (pode expandir depois)
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Página carregada com sucesso!");
+});
