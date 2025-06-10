6a567faeec19b3e4889cb0729d2640c83838c061
Rafael Lucena <berlimrafa.rf@gmail.com>
2025-06-10 00:01:52 -0300

    Adicionando função de menu "hamburguer" para celular script.js

--- meu-site/js/script.js
+++ meu-site/js/script.js
@@ -1,9 +1,12 @@
-// script.js
-
-// Mensagem de teste no console
 console.log("JavaScript funcionando!");
 
-// Exemplo de interação (pode expandir depois)
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Página carregada com sucesso!");
+
+  const toggleButton = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
 });
